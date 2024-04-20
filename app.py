6a372c22eff033114645118bb30df9cf701a903b
Cedric Nyagatare <163790819+NYGCEDRIC@users.noreply.github.com>
2024-04-20 15:52:40 -0700

    Update and rename app (1).py to app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# Copyright (c) Meta Platforms, Inc. and affiliates
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# MIT_LICENSE file in the root directory of this source tree.
-
 from __future__ import annotations
 
 import os
